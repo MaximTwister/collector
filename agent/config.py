da13a72536697a04c620d2cafdac58f093e4dc3b
Maksym Maksymchuk <twister.maxim@gmail.com>
2022-07-10 15:45:54 +0300

    10 July 2022 Lesson

--- agent/config.py
+++ agent/config.py
@@ -3,6 +3,8 @@ import socket
 
 from ruamel.yaml import YAML
 
+from metric import Meta
+
 
 class AgentConfig:
 
@@ -11,13 +13,14 @@ class AgentConfig:
     cpu = False
     memory = False
     hostname = socket.gethostname()
-    host_identificator = 0
+    host_id = 0
     agent_yaml = YAML()
     config = {}
 
     def __init__(self, configuration_file):
         self.__config_file = configuration_file
         self.parse_config()
+        self.meta: Meta = self.create_meta_object()
 
     def load_config_from_yaml(self) -> dict:
         with open(self.__config_file, "r") as f:
@@ -38,7 +41,7 @@ class AgentConfig:
     def transform_values(self, key, value):
         transformation_mapper = {
             "interval": self.transform_interval,
-            "host_identificator": self.transform_identificator
+            "host_id": self.transform_host_id,
         }
 
         transformator = transformation_mapper.get(key, None)
@@ -48,12 +51,12 @@ class AgentConfig:
         else:
             return value
 
-    def transform_identificator(self, value) -> str:
+    def transform_host_id(self, value) -> str:
         if value != 0:
             return value
         else:
             _uuid = str(uuid.uuid4())
-            self.config["host_identificator"] = _uuid
+            self.config["host_id"] = _uuid
             self.load_config_to_yaml(self.config)
             return _uuid
 
@@ -70,3 +73,11 @@ class AgentConfig:
 
         multiplier = multipliers.get(units)
         return int(interval_value) * multiplier
+
+    def create_meta_object(self) -> Meta:
+        meta_kwargs = {
+            "source_name": self.source_name,
+            "hostname": self.hostname,
+            "host_id": self.host_id
+        }
+        return Meta.parse_obj(meta_kwargs)
